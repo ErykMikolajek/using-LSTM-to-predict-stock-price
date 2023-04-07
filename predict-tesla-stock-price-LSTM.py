91536302ba7d621f7e741dedd6c9947851657d21
Eryk Mikołajek <mikolajek.eryk@gmail.com>
2023-04-07 19:38:59 +0200

    added data, validated

--- predict-tesla-stock-price-LSTM.py
+++ predict-tesla-stock-price-LSTM.py
@@ -1,39 +1,49 @@
-# Importing modules
-import numpy as np 
+from sklearn.metrics import mean_squared_error
+from keras.models import load_model
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.models import Sequential
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 import pandas as pd
 
-# Reading in the data
-tesla = pd.read_csv('TSLA.csv')
+# reading the Kaggle S&P 500 stock data
+apple_stock = pd.read_csv('AAPL_data.csv')
 
 # Isolating the date and close price
-tesla = tesla[['Date', 'Close']]
+apple_stock = apple_stock[['date', 'close']]
+print(apple_stock.head())
 
 # Selecting the stationary part of the dataset
 """
 Start date: 1/1/14 -> index = 884
 End date: 12/31/16 -> index = 1639
 """
-new_tesla = tesla.loc[884:1639]
+apple_stock = apple_stock.loc[884:1639]
 
 # Feature preprocessing
-new_tesla = new_tesla.drop('Date', axis = 1)
-new_tesla = new_tesla.reset_index(drop = True)
+new_tesla = new_tesla.drop('Date', axis=1)
+new_tesla = new_tesla.reset_index(drop=True)
 T = new_tesla.values
 T = T.astype('float32')
 T = np.reshape(T, (-1, 1))
 
 # Min-max scaling to get the values in the range [0,1] to optimize convergence speed
-from sklearn.preprocessing import MinMaxScaler
 
-scaler = MinMaxScaler(feature_range = (0, 1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 T = scaler.fit_transform(T)
 
 # 80-20 split
 train_size = int(len(T) * 0.80)
 test_size = int(len(T) - train_size)
-train, test = T[0:train_size,:], T[train_size:len(T),:]
+train, test = T[0:train_size, :], T[train_size:len(T), :]
 
 # Method for create features from the time series data
+
+
 def create_features(data, window_size):
     X, Y = [], []
     for i in range(len(data) - window_size - 1):
@@ -41,6 +51,7 @@ def create_features(data, window_size):
         X.append(window)
         Y.append(data[i + window_size, 0])
     return np.array(X), np.array(Y)
+
 
 # Roughly one month of trading assuming 5 trading days per week
 window_size = 20
@@ -58,28 +69,25 @@ train_shape = train.shape
 test_shape = test.shape
 
 # Make sure that the number of rows in the dataset = train rows + test rows
+
+
 def isLeak(T_shape, train_shape, test_shape):
     return not(T_shape[0] == (train_shape[0] + test_shape[0]))
+
 
 print(isLeak(T_shape, train_shape, test_shape))
 
 # Model imports
-import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-from keras.callbacks.callbacks import ModelCheckpoint
 
-# Setting seed for reproducibility 
+# Setting seed for reproducibility
 tf.random.set_seed(11)
 np.random.seed(11)
 
 # Building model
 model = Sequential()
 
-model.add(LSTM(units = 50, activation = 'relu', #return_sequences = True, 
-               input_shape = (X_train.shape[1], window_size)))
+model.add(LSTM(units=50, activation='relu',  # return_sequences = True,
+               input_shape=(X_train.shape[1], window_size)))
 model.add(Dropout(0.2))
 
 # Optional additional model layer to make a deep network. If you want to use this, uncomment #return_sequences param in previous add
@@ -90,26 +98,25 @@ model.add(Dropout(0.2))
 
 # Output layer
 model.add(Dense(1))
-model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+model.compile(loss='mean_squared_error', optimizer='adam')
 
 # Save models
 filepath = 'saved_models/model_epoch_{epoch:02d}.hdf5'
 
-checkpoint = ModelCheckpoint(filepath = filepath,
-                             monitor = 'val_loss',
-                             verbose = 1,
-                             save_best_only = True,
-                             mode ='min'
-                            )
+checkpoint = ModelCheckpoint(filepath=filepath,
+                             monitor='val_loss',
+                             verbose=1,
+                             save_best_only=True,
+                             mode='min'
+                             )
 
-history = model.fit(X_train, Y_train, epochs = 100, batch_size = 20, validation_data = (X_test, Y_test), 
-                    callbacks = [checkpoint], 
-                    verbose = 1, shuffle = False)
+history = model.fit(X_train, Y_train, epochs=100, batch_size=20, validation_data=(X_test, Y_test),
+                    callbacks=[checkpoint],
+                    verbose=1, shuffle=False)
 
 """
 Loading the best model and predicting
 """
-from keras.models import load_model
 
 best_model = load_model('saved_models/model_epoch_89.hdf5')
 
@@ -127,15 +134,14 @@ Y_hat_test = scaler.inverse_transform(test_predict)
 Y_test = scaler.inverse_transform([Y_test])
 Y_train = scaler.inverse_transform([Y_train])
 
-# Reshaping 
-Y_hat_train = np.reshape(Y_hat_train, newshape = 583)
-Y_hat_test = np.reshape(Y_hat_test, newshape = 131)
+# Reshaping
+Y_hat_train = np.reshape(Y_hat_train, newshape=583)
+Y_hat_test = np.reshape(Y_hat_test, newshape=131)
 
-Y_train = np.reshape(Y_train, newshape = 583)
-Y_test = np.reshape(Y_test, newshape = 131)
+Y_train = np.reshape(Y_train, newshape=583)
+Y_test = np.reshape(Y_test, newshape=131)
 
 # Model performance evaluation
-from sklearn.metrics import mean_squared_error
 
 train_RMSE = np.sqrt(mean_squared_error(Y_train, Y_hat_train))
 
